445124d2a33f22dbf0ba66bf26dce1c08e57ddbb
zhg <1640532428@qq.com>
2025-10-16 19:17:35 +0800

    fix: response to 404.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <h1>MY LIFE</h1>
+  <div class="flex flex-1 overflow-hidden bg-gray-50 py-16 leading-8 dark:bg-gray-900">
+    <div class="relative mx-auto h-full w-full max-w-screen-2xl overflow-y-auto bg-gray-50 dark:bg-gray-900 dark:text-white">
+      <RouterView />
+    </div>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const router = createRouter({
+  // 改成 Hash 模式
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+})
+app.use(router)
+app.mount('#app')
