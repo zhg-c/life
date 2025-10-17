be6379ef42a21d7e0183fc3f72255743d8119072
zhg <1640532428@qq.com>
2025-10-17 13:46:01 +0800

    fix: imported uno.css in main.ts,otherwise unocss will not take effect.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
+import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
